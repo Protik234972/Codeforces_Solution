f44b9d0cdab70800e1b9cb5f3def69c56a8c86b7
Protik234972 <baruaprotik234972@gmail.com>
2024-01-30 19:26:37 +0600

    Updated

--- 1100 Rating/Laptop.py
+++ 1100 Rating/Laptop.py
@@ -1,12 +1,8 @@
 n = int(input())
-price = []
-quality = []
 while n !=0:
-    a,b = map(int,input().split())
-    price.append(a)
-    quality.append(b)
+    a,b = map(int, input().split())
+    if a>b:
+        print("Happy Alex")
+        exit()
     n-=1
-if price.index(min(price)) ==  quality.index(max(price)):
-    print("Happy Alex")
-else:
-    print("Poor Alex")
+print("Poor Alex")

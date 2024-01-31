271947e7a642ede16170da1e68eec7f201c84cef
Protik234972 <baruaprotik234972@gmail.com>
2024-02-01 01:02:17 +0600

    commit

--- 1000 Rating/cAPS lOCK.py
+++ 1000 Rating/cAPS lOCK.py
@@ -4,9 +4,7 @@ flag = True
 for i in range(1,len(s)):
     if s[i].islower():
         flag = False
-
-
-    
+  
 if flag != True:
     print(s)
 elif flag == True and s[0].isupper():
---
+++ 1300 Rating/Cut Ribbon.py
@@ -0,0 +1,5 @@
+a= 5
+b= 4
+c= 3
+s = max(a,b,c)
+print(s)

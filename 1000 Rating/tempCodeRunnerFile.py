f670bd9eefa6afc704f3a19087b63e1c0d452c8e
Protik234972 <baruaprotik234972@gmail.com>
2024-01-31 23:34:13 +0600

    commit

---
+++ 1000 Rating/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+s = input()
+flag = True
+
+for i in range(1,len(s)):
+    if s[i].islower():
+        flag = False
+
+
+    
+if flag != True:
+    print(s)
+elif flag == True and s[0].isupper():
+    print(s.lower())
+else:
+    print(s.title())
